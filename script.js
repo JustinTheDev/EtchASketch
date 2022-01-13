3ccdfcf4baf75bbfa72de5b4eecf5e3b5d8fb211
Justin <Justin.TheDev@gmail.com>
2022-01-13 13:23:58 -0600

    Created the ability to generate 16 divs within the main sketch div.  Some general styling.

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ const settingsContainer = document.querySelector("#settings-container");
 console.log(mainContainer);
 console.log(sketchContainer);
 console.log(settingsContainer);
+
+for (let i = 0; i<16; i++)
+{
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute("id","sketch-pixel");
+    sketchContainer.appendChild(newDiv);
+
+    
+}
