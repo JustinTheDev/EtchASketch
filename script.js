ae790c08e080fa3febdbbd1a3fe69928d870f4e6
Justin <Justin.TheDev@gmail.com>
2022-01-13 12:06:29 -0600

    Adding some core variables to the JS, some base styling to the CSS and a little more HTMl for structure.

---
+++ script.js
@@ -0,0 +1,7 @@
+const mainContainer = document.querySelector("#main-container");
+const sketchContainer = document.querySelector("#sketch-container");
+const settingsContainer = document.querySelector("#settings-container");
+
+console.log(mainContainer);
+console.log(sketchContainer);
+console.log(settingsContainer);
