58fcc2e3192024b54a7d409e5ebea250845d7b4e
Justin <Justin.TheDev@gmail.com>
2022-01-13 19:34:04 -0600

    Basic 16x16 grid and clear function are working.  Started interface for resolution adjustment

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ const mainContainer = document.querySelector("#main-container");
 const sketchContainer = document.querySelector("#sketch-container");
 const settingsContainer = document.querySelector("#settings-container");
 
-console.log(mainContainer);
-console.log(sketchContainer);
-console.log(settingsContainer);
-
 for (let i = 0; i<(16*16); i++)
 {
     let divHeight = sketchContainer.clientHeight/16;
@@ -15,6 +11,19 @@ for (let i = 0; i<(16*16); i++)
     newDiv.style.height = divHeight + "px";
     newDiv.style.width = divWidth + "px";
     sketchContainer.appendChild(newDiv);
-
-    
 }
+
+let divArray = document.querySelectorAll("#sketch-pixel");
+
+divArray.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+        this.style.backgroundColor = "black";
+    });
+});
+
+document.getElementById("clear").addEventListener("click", function(){
+    let arr = document.querySelectorAll("#sketch-pixel");
+    arr.forEach(function(elem){
+        elem.style.backgroundColor="white";
+    });
+});
